9d08c532f28aff87116ae2a46a0d0c75cdf95f8b
Dark7lord <budda185@mail.ru>
2020-06-14 00:21:05 +0900

    Добавил карту Яндекс со скриптами с кастомным маркером (не то, чтобы Google плох, но у наших документация по русски более понятная =))

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+// Карта Яндекс с кастомным маркером
+
+ymaps.ready(function () {
+  var myMap = new ymaps.Map('map', {
+          center: [59.938635, 30.323118],
+          zoom: 17
+      }, {
+          searchControlProvider: 'yandex#search'
+      }),
+
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+          '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+      ),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+          hintContent: 'Собственный значок метки',
+          balloonContent: 'Студия Нёрдс'
+      }, {
+          iconLayout: 'default#image',
+          iconImageHref: 'img/map-marker.png',
+          iconImageSize: [231, 190],
+          iconImageOffset: [-60, -170]
+      });
+
+  myMap.geoObjects.add(myPlacemark);
+  myMap.behaviors.disable('scrollZoom'); // Убрать прокрутку карты колесиком мыши
+});
