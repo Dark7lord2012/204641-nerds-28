1b6604fbe3d3e6219c03eb1c9aae2a993561d12b
Dark7lord <budda185@mail.ru>
2020-06-15 20:54:46 +0900

    Добавил скрипты для модального окна (открытие, закрытие, анимация, неотправка при пустом поле)

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // Карта Яндекс с кастомным маркером
 
 ymaps.ready(function () {
-  var myMap = new ymaps.Map('map', {
+  let myMap = new ymaps.Map('map', {
           center: [59.938635, 30.323118],
           zoom: 17
       }, {
@@ -25,4 +25,75 @@ ymaps.ready(function () {
   myMap.geoObjects.add(myPlacemark);
   myMap.behaviors.disable('scrollZoom'); // Убрать прокрутку карты колесиком мыши
   myMap.controls.remove('searchControl'); // Удалил поисковую строку, так как оно частично перекрывается контактами
+});
+
+
+// Форма "Напишите нам"
+
+let btnOpenModal = document.querySelector('.map__btn-open-modal');
+let modalCallback = document.querySelector('.modal-callback');
+let btnCloseModal = document.querySelector('.modal-callback__btn-close');
+let inputName = document.querySelector('.modal-callback__label--name');
+let inputEmail = document.querySelector('.modal-callback__label--email');
+let inputComment = document.querySelector('.modal-callback__textarea');
+let btnSubmitModal = document.querySelector('.modal-callback__btn-submit');
+
+let isStorageSupport = true;
+let storageName = "";
+let storageEmail = "";
+
+try {
+  storageName = localStorage.getItem('name');
+  storageEmail = localStorage.getItem('email');
+} catch(error) {
+  isStorageSupport = false;
+}
+
+btnOpenModal.addEventListener('click', function(event) {
+  event.preventDefault();
+  modalCallback.classList.add('modal-callback--opened');
+  inputName.focus();
+
+  if (storageName) {
+    inputName.value = storageName; // не работат в Chrome, но фокус на поле срабатывает
+    inputEmail.focus();
+  } else {
+    inputName.focus();
+  }
+
+  if (storageEmail) {
+    inputEmail.value = storageEmail; // тоже не работат в Chrome, но фокус на поле срабатывает
+    inputComment.focus();
+  }
+});
+
+btnCloseModal.addEventListener('click', function(event) {
+  modalCallback.classList.remove('modal-callback--opened');
+  modalCallback.classList.remove('modal-callback--error');
+});
+
+window.addEventListener('keydown', function(event) {
+  if (event.keyCode === 27) {
+    if (modalCallback.classList.contains('modal-callback--opened')) {
+      event.preventDefault();
+      modalCallback.classList.remove('modal-callback--opened');
+      modalCallback.classList.remove('modal-callback--error');
+    }
+  }
+} );
+
+btnSubmitModal.addEventListener('click', function(event) {
+  if (! inputName.value || ! inputEmail.value || ! inputComment.value) {
+    console.log('Bro!');
+    event.preventDefault();
+    modalCallback.classList.remove('modal-callback--error');
+    modalCallback.offsetWidth = modalCallback.offsetWidth; // не понимаю этот хак =(
+    modalCallback.classList.add('modal-callback--error');
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem('name', inputName.value);
+      localStorage.setItem('email', inputEmail.value);
+      console.log('Bruh');
+    }
+  }
 });
